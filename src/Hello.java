97ed75a6706601dc0e3fa55e46642f5d4bc7c7d0
Elena Poposka <elena.poposka01@gmail.com>
2026-04-08 18:56:32 +0200

    store changes

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,Tim");
+        boolean isAlien = true;
+        if(isAlien == false)
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+
+        boolean isCar=false;
+        if(isCar != false)
+        {
+            System.out.println("This is not supposed");
+        }
+    }
+}
